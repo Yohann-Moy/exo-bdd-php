f13262247d735984f18a9f68dab3693d9708b9f9
unknown <yohann.moy@gmail.com>
2023-04-05 22:43:09 +0200

    Push de l'exercice

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TP Amazoone pour mes stagiaires de Soissons</title>
+    <meta name="description" content="TP Amazoone pour mes stagiaires de Soissons">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <p class="logo">Amazoone</p>
+            <ul>
+                <li><a href="#">Flash</a></li>
+                <li><a href="#">Basics</a></li>
+                <li><a href="#">Cart. cadeaux</a></li>
+                <li><a href="#">Coupons</a></li>
+                <li><a href="#">Nouveau</a></li>
+                <li><a href="#">Business</a></li>
+                <li><a href="#">Beauté</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h1>Ventes flash et promotions</h1>
+        <div class="container">
+
+
+            <!-- 
+                Modèle de carte HTML à afficher pour chaque article issu de la BDD
+                en prenant soin de mettre à jour : 
+                - l'illustration, 
+                - la description de l'image (alt)
+                - le prix
+                - le nom du produit
+
+                Bon courage ! :)
+            -->
+            <div class="card">
+                <img src="https://m.media-amazon.com/images/I/71bnkM5q8CL._AC_UL600_QL65_.jpg" alt="illustration de Echo Doute">
+                <p class="info">25.99€&nbsp;-&nbsp;Vente flash</p>
+                <p class="product">Echo Doute</p>
+            </div>
+            <!-- Fin du modèle de carte -->
+
+            
+        </div>
+    </main>
+    <footer>
+        <p>Yohann MOY - TP Amazoone pour mes stagiaires de Soissons</p>
+    </footer>
+</body>
+</html>
